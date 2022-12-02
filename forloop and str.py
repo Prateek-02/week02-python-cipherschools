bc5d4bd30178a2992aaa140e9432feafead679f8
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-12-02 10:15:10 +0530

    forloop.py

---
+++ forloop and str.py
@@ -0,0 +1,14 @@
+# name = "Prateek"
+# for i in range(len(name)):
+#     print(name[i])
+    
+    
+# name = "Prateek"
+# for i in name:
+#     print(i)    
+
+num = input("enter a number: ")
+sum = 0
+for i in num:
+    sum += int(i)
+print(sum)    
