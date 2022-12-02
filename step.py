e2df462ae32074d256aee79b7482792b159302a3
Prateek-02 <Prateekrajgrd74@gmail.com>
2022-12-02 09:59:23 +0530

    step.py

---
+++ step.py
@@ -0,0 +1,10 @@
+for i in range(1,11,2):             # (1,11,2) here 2 is step argument means the number will be printed after skipping one element  output = 1,3,5,7,9
+    print(i)
+
+
+
+# print number in reverse order
+
+for i in range (10,0,-1):
+    print(i)
+    
