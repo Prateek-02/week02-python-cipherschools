d1e889a399691d3beecaf3e791f0223da0f059db
Prateek <Prateekrajgrd74@gmail.com>
2022-12-09 23:56:42 +0530

    some_methods.py

---
+++ some_methods.py
@@ -0,0 +1,16 @@
+#count
+#sort method
+#sorted function
+#reverse
+#clear
+#copy
+
+fruits = ['apple','orange','banana','grapes','kiwi','apple','banana']
+# print(fruits.count("apple"))                                    #it will print the count of apple how much time it is present in list
+# fruits.sort()                                                   #it will arrange the items in alphabetical order            
+num = [3,5,1,9,10]
+# num.sort()                                                        #it will arrange the items in asscending order           
+# print(num)
+print(fruits)   
+num.clear()
+print(num)
