4d9a8c3c306ae04dfa3f11729631f0ee62394dcb
Prateek <Prateekrajgrd74@gmail.com>
2022-12-09 23:35:05 +0530

    delete_mrthods.py

---
+++ delete_methods.py
@@ -0,0 +1,14 @@
+#common methods to delete data from list
+fruits = ['apple','orange','mango','pear','banana','kiwi']
+#pop method
+# fruits.pop()                            #if you did not pass any argument in pop it will delete the last element by default
+# fruits.pop(1)                           #it will pop out the 1st index element
+# print(fruits)
+
+#del 
+# del fruits[1]                             #it will delete the element at 1st index
+# print(fruits)
+
+#remove
+fruits.remove("banana")
+print(fruits)
