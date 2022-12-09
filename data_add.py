83c573d0be438980da331c5c25b1973e822f25bc
Prateek <Prateekrajgrd74@gmail.com>
2022-12-09 23:22:59 +0530

    data_add.py

---
+++ data_add.py
@@ -0,0 +1,15 @@
+#some more methods to add data in our list
+#insert method
+#how to concatenate two list
+#extend method
+#difference b/w append and extend
+
+fruits1 = ["mango","orange"]
+# fruits1.insert(1,"grapes")                 #it will insert grapes at 1st index,which is after mang0
+# print(fruits1)
+
+fruits2 = ["apple","grapes"]
+
+# fruits1.extend(fruits2)                     #it will add two lists
+fruits1.append(fruits2)
+print(fruits1)
