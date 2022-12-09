b360456708b3f1e85e4301868f6d8349713eef2b
Prateek <Prateekrajgrd74@gmail.com>
2022-12-10 00:13:35 +0530

    join_split.py

---
+++ join_split.py
@@ -0,0 +1,11 @@
+#split method
+#convert string to list
+
+name,age = 'Prateek,19'.split(",")
+print(name)
+print(age)
+
+#join method
+#convert list to string
+user_info = ['Prateek','19']
+print(','.join(user_info))
