b1ec6515704d578277fa9c96c8b3dda6b48be7f0
Prateek <Prateekrajgrd74@gmail.com>
2022-12-10 00:04:28 +0530

    compare_list.py

---
+++ compare_list.py
@@ -0,0 +1,6 @@
+fruits1 = ['apple','pear','orange']
+fruits2 = ['banana','kiwi','apple','banana']
+fruits3 = ['apple','pear','orange']
+print(fruits1 == fruits2)                       #it will print "False" 
+print (fruits1 == fruits3)                      #it will print "True"
+print (fruits1 is fruits3)                      #it will print "False" because fruits1 is diff. object and fruits3 is diff. object
