bf30b4a27974a515b4a675f43c739637ceb53023
Prateek <Prateekrajgrd74@gmail.com>
2022-12-11 22:36:03 +0530

    update_dict.py

---
+++ update_dict.py
@@ -0,0 +1,10 @@
+user_info = {
+    'name' : 'Prateek',
+    'age' : 19,
+    'fav_movies' : ['coco','kimi no na wa'],
+    'fav_tunes' : ['awakening','fairy tail'],
+}
+more_info = {'state' : 'haryana','hobbies' : ['coding','reading','guitar']}
+
+user_info.update(more_info)
+print(user_info)
